b66d846082d78d7e0f05050638a0eaf95e48e995
littlemm <785897146@qq.com>
2017-10-30 23:58:05 +0800

    日常

---
+++ NetService/ServiceCore/Migrations/20171028164919_stuDB.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ServiceCore.Migrations
+{
+    public partial class stuDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Classes",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ClassInfo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClassName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DATA_StudentId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Teacher = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    section = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classes", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Classes_Students_DATA_StudentId",
+                        column: x => x.DATA_StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classes_DATA_StudentId",
+                table: "Classes",
+                column: "DATA_StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Classes");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
